2c22a8681ed97a88eb9c13232602844e83ae6122
David Duarte <jesusdavidduartejimenez@gmail.com>
2021-11-19 07:30:49 -0500

    🚀⚙️ header 🚀⚙️

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
+import React, { Fragment, useState } from 'react';
+import Cart from './components/Cart/Cart';
+
+import Header from './components/Layout/Header';
+import Meals from './components/Meals/Meals';
+import Example from './components/UI/Example';
+
 function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+  const shoCartHandler = () => {
+    setCartIsShown(true);
+  };
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  };
   return (
-    <div>
-      <h2>Let's get started!</h2>
-    </div>
+    <Fragment>
+      {cartIsShown && <Cart />}
+      <Header onShowCart={shoCartHandler} onHideCart={hideCartHandler}></Header>
+      <main>
+        <Meals />
+      </main>
+    </Fragment>
   );
 }
 
